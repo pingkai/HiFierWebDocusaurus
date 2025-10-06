c82918f1cc8f22ed63605106be84a62ff5afe248
Ping Kai <pingkai010@gmail.com>
2025-10-05 23:09:10 -0400

    docs: add connections.md
    Signed-off-by: Ping Kai <pingkai010@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,11 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
     i18n: {
       defaultLocale: "en",
       locales: ["en", "zh-Hans"],
+      localeConfigs: {
+        "zh-Hans": {
+          label: "简体中文",
+        },
+      },
     },
 
     presets: [
